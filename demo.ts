f887d1fec989284308f4e7dd1608b392cb3bb25d
Lucy <lucyduss@outlook.com>
2026-05-23 11:29:28 +0200

    add dice rolling functions and createBoard utility with tests

--- demo.ts
+++ demo.ts
@@ -3,3 +3,15 @@ export function formattedAddition(a: number, b: number): string {
 }
 
 console.log(formattedAddition(3, 2));
+
+export function rollDie(): number {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+export function rollDice(): number {
+  return rollDie() + rollDie();
+}
+
+export function createBoard(): number[] {
+  return [1,2,3,4,5,6,7,8,9];
+}
--- demo_test.ts
+++ demo_test.ts
@@ -1,5 +1,5 @@
 import {assertEquals} from "@std/assert";
-import {formattedAddition,} from "./demo.ts";
+import {formattedAddition, rollDie, rollDice, createBoard} from "./demo.ts";
 
 Deno.test("3 + 5 = 8", function addTest() {
   // Arrange
@@ -24,3 +24,20 @@ Deno.test("3 + -5 = -2", function addTest() {
   // Then
   assertEquals(actual, "3 + -5 = -2");
 });
+
+Deno.test("Würfel liefert eine Zahl zwischen 1 bis 6", function () {
+  const result = rollDie();
+  const actual = result >= 1 && result <= 6;
+  assertEquals(actual, true);
+})
+
+Deno.test("Zwei Würfel ergeben eine Summe zwischen 2 und 12", function () {
+  const result = rollDice();
+  const actual = result >= 2 && result <= 12;
+  assertEquals(actual, true);
+})
+
+Deno.test("Knobelscheit startet mit Board 1 bis 9", function () {
+  const board = createBoard();
+  assertEquals(board, [1,2,3,4,5,6,7,8,9])
+})
